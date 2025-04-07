55e2ea78a0606775c535ce95c23b6acac52169f7
iocafe87 <iocafe87@gmail.com>
2025-04-08 00:07:30 +0800

    feat: 2025-4-8

--- script/world.gd
+++ script/world.gd
@@ -4,8 +4,19 @@ extends Node2D
 @onready var ball: Ball = $Ball
 @onready var death_block: CollisionShape2D = $DeathBlock/CollisionShape2D
 
-## 平台左右移动的速度系数
-const SPEED: float = 100.0
+var rng = RandomNumberGenerator.new()
+
+## 平台和ball左右移动的速度系数
+@export var MOVE_SPEED: float = 500.0
+## ball飞行的速度
+@export var BALL_SPEED: float = 30000.0
+## ball是否已经发射
+var isShoot: bool = false
+## 斜上45度或135度的方向
+var i: int = 0
+var deg: float = 45.0
+var angle: float = 0.0
+
 
 func _ready() -> void:
 	## 获取屏幕的宽度
@@ -28,4 +39,31 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	## 左右移动横轴的差值
+	var moveAxis: float = Input.get_axis("move_left", "move_right")
+	## 移动的趋势
+	var trend: float = moveAxis * delta * MOVE_SPEED
+	## 平台左右移动
+	platform.position.x = platform.position.x + trend
+	if not isShoot:
+		## 如果ball还未发射过，那么也可以跟着平台一起移动
+		ball.position.x = ball.position.x + trend
+		
+	## 小球开始移动了
+	if isShoot:
+		## 方向向量
+		var dir: Vector2 = Vector2(cos(angle), -sin(angle))
+		ball.velocity = dir * BALL_SPEED * delta
+		ball.move_and_slide()
+		
+func _unhandled_input(event: InputEvent) -> void:
+	## 处理发射事件
+	if event.is_action_pressed("shoot") and not isShoot:
+		## 先把是否第一次发射，改为true
+		isShoot = true
+		## 随机定义符号
+		var i : int = rng.randi_range(0, 1)
+		## 按 45度 or 135度 角发射出去，转为弧度
+		deg = 45.0 if i else 135.0
+		angle = deg_to_rad(deg)
+		
