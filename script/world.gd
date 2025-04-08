fe625017fdcd83879d3052c06edd53d01d53b76f
iocafe87 <iocafe87@gmail.com>
2025-04-08 10:18:33 +0800

    增加基于碰撞时法线的反弹方向向量的移动

--- script/world.gd
+++ script/world.gd
@@ -9,13 +9,15 @@ var rng = RandomNumberGenerator.new()
 ## 平台和ball左右移动的速度系数
 @export var MOVE_SPEED: float = 500.0
 ## ball飞行的速度
-@export var BALL_SPEED: float = 30000.0
+@export var BALL_SPEED: float = 500.0
 ## ball是否已经发射
 var isShoot: bool = false
 ## 斜上45度或135度的方向
 var i: int = 0
 var deg: float = 45.0
 var angle: float = 0.0
+## 移动的向量
+var dir: Vector2 = Vector2.ZERO
 
 
 func _ready() -> void:
@@ -48,13 +50,24 @@ func _process(delta: float) -> void:
 	if not isShoot:
 		## 如果ball还未发射过，那么也可以跟着平台一起移动
 		ball.position.x = ball.position.x + trend
-		
+			
+func _physics_process(delta: float) -> void:
 	## 小球开始移动了
 	if isShoot:
-		## 方向向量
-		var dir: Vector2 = Vector2(cos(angle), -sin(angle))
+		
 		ball.velocity = dir * BALL_SPEED * delta
-		ball.move_and_slide()
+		var coll: KinematicCollision2D = ball.move_and_collide(ball.velocity)
+		
+		## 碰撞检测
+		if coll:
+			## 获取碰撞点的法线
+			var nor: Vector2 = coll.get_normal()
+			## 以法线为中线的反向向量
+			dir = ball.velocity.bounce(nor).normalized()
+			
+			ball.velocity = dir * BALL_SPEED * delta
+			ball.move_and_collide(ball.velocity)
+		
 		
 func _unhandled_input(event: InputEvent) -> void:
 	## 处理发射事件
@@ -66,4 +79,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		## 按 45度 or 135度 角发射出去，转为弧度
 		deg = 45.0 if i else 135.0
 		angle = deg_to_rad(deg)
+		## 第一次的方向向量
+		dir = Vector2(cos(angle), -sin(angle)).normalized()
 		
