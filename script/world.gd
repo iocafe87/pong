60aa53280a20ce0bc136be8876a8ee52627accf4
iocafe87 <iocafe87@gmail.com>
2025-04-10 00:11:58 +0800

    完善死亡倒计时、音效等

--- script/world.gd
+++ script/world.gd
@@ -3,6 +3,11 @@ extends Node2D
 @onready var platform: Platform = $Platform
 @onready var ball: Ball = $Ball
 @onready var death_block: CollisionShape2D = $DeathBlock/CollisionShape2D
+@onready var ball_pop_up: AudioStreamPlayer2D = $Audio/BallPopUp
+@onready var brick_destroy: AudioStreamPlayer2D = $Audio/BrickDestroy
+@onready var ball_explosion: AudioStreamPlayer2D = $Audio/BallExplosion
+@onready var death_wait: Timer = $DeathWait
+
 
 var rng = RandomNumberGenerator.new()
 
@@ -19,11 +24,21 @@ var angle: float = 0.0
 ## 移动的向量
 var dir: Vector2 = Vector2.ZERO
 
+## 屏幕宽高
+var screenWidth: int = 0
+var screenHeight: int = 0
+
+## 移动limit
+var limit: int = 50
+
+## 是否死亡
+var isDead: bool = false
+
 
 func _ready() -> void:
 	## 获取屏幕的宽度
-	var screenWidth: int = get_viewport().size.x
-	var screenHeight: int = get_viewport().size.y
+	screenWidth = get_viewport().size.x
+	screenHeight = get_viewport().size.y
 	
 	## 初始化Platform的位置
 	var platformPosition: Vector2 = Vector2(screenWidth * 0.5, screenHeight * 0.95)
@@ -41,37 +56,49 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	## 左右移动横轴的差值
-	var moveAxis: float = Input.get_axis("move_left", "move_right")
-	## 移动的趋势
-	var trend: float = moveAxis * delta * MOVE_SPEED
-	## 平台左右移动
-	platform.position.x = platform.position.x + trend
-	if not isShoot:
-		## 如果ball还未发射过，那么也可以跟着平台一起移动
-		ball.position.x = ball.position.x + trend
+	pass
 			
 func _physics_process(delta: float) -> void:
-	## 小球开始移动了
-	if isShoot:
+	if not isDead:
+		## 左右移动横轴的差值
+		var moveAxis: float = Input.get_axis("move_left", "move_right")
+		## 移动的趋势
+		var trend: float = moveAxis * delta * MOVE_SPEED
 		
-		ball.velocity = dir * BALL_SPEED * delta
-		var coll: KinematicCollision2D = ball.move_and_collide(ball.velocity)
-		
-		## 碰撞检测
-		if coll:
-			## 获取碰撞点的法线
-			var nor: Vector2 = coll.get_normal()
-			## 以法线为中线的反向向量
-			dir = ball.velocity.bounce(nor).normalized()
+		var newX: float = platform.position.x + trend
+
+		## 限定范围
+		if newX >= limit and newX <= screenWidth - limit:
+			## 平台左右移动
+			platform.position.x = newX
 			
+			## ball未发射前，跟随
+			if not isShoot:
+				## 如果ball还未发射过，那么也可以跟着平台一起移动
+				ball.position.x = ball.position.x + trend
+				
+		## 小球开始移动了
+		if isShoot:
 			ball.velocity = dir * BALL_SPEED * delta
-			ball.move_and_collide(ball.velocity)
+			var coll: KinematicCollision2D = ball.move_and_collide(ball.velocity)
+			
+			## 碰撞检测
+			if coll:
+				## 播放碰撞音效
+				ball_pop_up.play()
+				
+				## 获取碰撞点的法线
+				var nor: Vector2 = coll.get_normal()
+				## 以法线为中线的反向向量
+				dir = ball.velocity.bounce(nor).normalized()
+				
+				ball.velocity = dir * BALL_SPEED * delta
+				ball.move_and_collide(ball.velocity)
 		
 		
 func _unhandled_input(event: InputEvent) -> void:
 	## 处理发射事件
-	if event.is_action_pressed("shoot") and not isShoot:
+	if event.is_action_pressed("shoot") and not isShoot and not isDead:
 		## 先把是否第一次发射，改为true
 		isShoot = true
 		## 随机定义符号
@@ -81,4 +108,25 @@ func _unhandled_input(event: InputEvent) -> void:
 		angle = deg_to_rad(deg)
 		## 第一次的方向向量
 		dir = Vector2(cos(angle), -sin(angle)).normalized()
+		
+
+
+func _on_death_block_body_entered(body: Node2D) -> void:
+	## 先对body的类型进行判断
+	if body is Ball:
+		## 转换
+		var ball: Ball = body
+		ball.queue_free()
+		
+		## 设置死亡
+		isDead = true
+		## 播放死亡音效
+		ball_explosion.play()
+		## 死亡冻结开始计时
+		death_wait.start()
+
+
+func _on_death_wait_timeout() -> void:
+	## 计时器播放完毕后回调
+	get_tree().reload_current_scene()
 		
